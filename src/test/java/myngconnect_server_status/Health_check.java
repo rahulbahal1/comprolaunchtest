3cd2ac0be35749d3a713ee84a187900bf2e198eb
rahulbahal <rbahal91@gmail.com>
2014-02-11 11:10:43 +0530

    repository renamed

---
+++ src/test/java/myngconnect_server_status/Health_check.java
@@ -0,0 +1,27 @@
+package myngconnect_server_status;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.compro.automation.utils.CSVHandler;
+
+public class Health_check {
+		CSVHandler login_details = null;
+		public void health_login(RemoteWebDriver driver ) throws Exception {
+    	//String b = Integer.toString(i);
+    	login_details = new CSVHandler("src/test/resources/login_health_check.csv");
+		String baseurl  = login_details.getElementXpath("baseurl0");
+    	String usertype = login_details.getElementXpath("usertype");
+    	String username = login_details.getElementXpath("username");
+    	String password = login_details.getElementXpath("password");
+		driver.get(baseurl + "/login/teacher/login.spr");
+		System.out.println(usertype);
+		synchronized (driver){driver.wait(5000);}	
+
+	    driver.findElement(By.id("j_username")).clear();
+		driver.findElement(By.id("j_username")).sendKeys(username);
+		driver.findElement(By.id("passwordField")).clear();
+	    driver.findElement(By.id("passwordField")).sendKeys(password);
+	    driver.findElement(By.id("imgLogin")).click();
+	    synchronized (driver) {driver.wait(10000);}
+ }
+}
